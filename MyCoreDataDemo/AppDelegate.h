7ffdbc6b239c72839362958c8777823e89ac3136
ucan <ucanhp@126.com>
2012-11-16 16:50:03 +0800

    Initial Commit

---
+++ MyCoreDataDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyCoreDataDemo
+//
+//  Created by ucan on 12-11-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
